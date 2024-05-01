284a316ae0a4cab86b77d100aff5e934df5a50ed
Caceresenzo <caceresenzo1502@gmail.com>
2024-05-01 16:49:01 +0200

    pass 2nd stage

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,12 @@
 import * as net from 'net';
 
 const server = net.createServer((socket) => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
     socket.end();
 });
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log("codecrafters build-your-own-http");
 
-// Uncomment this to pass the first stage
 server.listen(4221, 'localhost', () => {
-    console.log('Server is running on port 4221');
+    console.log('server is running on port 4221');
 });
